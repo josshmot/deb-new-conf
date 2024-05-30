ae3c85c22fe924918241a0fc80366ea15d47fa96
josshmot <jrichard.smith98@gmail.com>
2024-05-30 17:17:19 +1000

    implemented logging

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+# Init log file and write timestamp
+logfile=/tmp/deb-new-conf.log
+date > $logfile
+
 # Check we aren't in root
 if [[ $(id -u) = 0 ]]
 then
     echo -e "!! Do not run this script as root! Instead run as some user, and the script will elevate itself when required."
+    echo -e "Script failed: ran run.sh as root" >> $logfile
     exit
 fi
 
@@ -12,8 +17,9 @@ cd $(dirname "$0")
 echo -e "Acquiring user directory..."
 home=$(readlink -f ~/)
 echo -e "-> User directory is: '$home'"
+echo -e "Home directory: $home" >> $logfile
 
 # Elevate to root and run primary script in bash so that we can use our fancy bash commands
 echo -e ""
-echo -e "This script now requires root access via sudo to install components. Please enter your password if requested."
-sudo bash ./sh/debian-config.sh $home
+echo -e "This script requires root access via sudo to install components. Please enter your password if requested."
+sudo bash ./sh/debian-config.sh $home $logfile
