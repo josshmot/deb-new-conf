39d9dc969228f61c53ca19ae17e14987a13b0118
josshmot <jrichard.smith98@gmail.com>
2024-05-30 21:00:33 +1000

    removed tee from run.sh, as we don't need it

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 # Init log file and write timestamp
 logfile=/tmp/deb-new-conf.log
-echo "" | tee -a $logfile &>/dev/null
-date | tee -a $logfile &>/dev/null
+echo "" &>> $logfile
+date &>> $logfile
 
 # Check we aren't in root
 if [[ $(id -u) = 0 ]]
 then
     echo -e "!! Do not run this script as root! Instead run as some user, and the script will elevate itself when required."
-    echo -e "Script failed: ran run.sh as root" | tee -a $logfile &>/dev/null
+    echo -e "Script failed: ran run.sh as root" &>> $logfile
     exit
 fi
 
@@ -18,7 +18,7 @@ cd $(dirname "$0")
 echo -e "Acquiring user directory..."
 home=$(readlink -f ~/)
 echo -e "-> User directory is: '$home'"
-echo -e "Home directory: $home" | tee -a $logfile &>/dev/null
+echo -e "Home directory: $home" &>> $logfile
 
 # Specify templogfile path
 templogfile=/tmp/deb-new-conf.log.tmp # We need this, as we can't write to the currently open log file from within debian-config.sh
@@ -32,7 +32,7 @@ configresult=$?
 # copy temp log file contents into log file (only if required)
 if [[ $configresult != 2 ]]
 then
-    cat $templogfile | tee -a $logfile &>/dev/null
+    cat $templogfile &>> $logfile
 fi
 
 if [[ $configresult != 0 ]]
